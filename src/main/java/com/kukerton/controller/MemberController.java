3a5a2571f327364daff190ba80c790ba6b986c2c
leesangmin <dkwkghkdlxld2@gmail.com>
2024-04-06 19:42:34 +0900

    feat: 카카오 소셜 로그인 추가

--- src/main/java/com/kukerton/controller/MemberController.java
+++ src/main/java/com/kukerton/controller/MemberController.java
@@ -5,10 +5,9 @@ import com.kukerton.global.response.BfResponse;
 import com.kukerton.service.MemberService;
 import java.util.Map;
 import lombok.RequiredArgsConstructor;
-
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -20,9 +19,8 @@ public class MemberController {
 
     private final MemberService memberService;
 
-    @PostMapping("/login")
+    @GetMapping ("/login")
     public ResponseEntity<BfResponse<?>> login(@RequestParam(name = "code") String code) {
-
         return ResponseEntity.status(HttpStatus.OK)
             .body(new BfResponse<>(GlobalSuccessCode.CREATE,
                 Map.of("id", memberService.getUserInfoFromResourceServer(code))));
