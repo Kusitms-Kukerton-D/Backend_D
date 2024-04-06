63fca3a07f816282ab5c43653c4a3e56a846f1ea
leesangmin <dkwkghkdlxld2@gmail.com>
2024-04-06 16:11:40 +0900

    feat: member 추가

---
+++ src/main/java/com/kukerton/global/TestController.java
@@ -0,0 +1,21 @@
+package com.kukerton.global;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequestMapping("/test")
+@RequiredArgsConstructor
+@RestController
+@Slf4j
+public class TestController {
+
+    private final MemberRepository memberRepository;
+    @GetMapping
+    public String test(){
+        log.info("controller 실행");
+        return memberRepository.findById(1L).get().getName();
+    }
+}
